3b8d891d34cd82d55e2b068bd553d5e7f87fa4c0
Dealermap <162360988+Dealermap@users.noreply.github.com>
2024-03-05 21:22:54 +0100

    script.js

--- script.js
+++ script.js
@@ -1,17 +1,41 @@
-var map = L.map('map', {
-    crs: L.CRS.Simple,
-    minZoom: -5
-});
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Los Santos Karte</title>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css"/>
+    <style>
+        #map {
+            height: 800px;
+            width: 100%;
+        }
+    </style>
+</head>
+<body>
 
-var bounds = [[0, 0], [8192, 8192]]; // Bildgröße
+<div id="map"></div>
 
-L.imageOverlay('https://www.gtavision.com/images/content/gtav/maps/grand-theft-auto-v_karte-satellit_hq.jpg', bounds).addTo(map);
+<script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
+<script>
+    var map = L.map('map', {
+        crs: L.CRS.Simple,
+        minZoom: -5
+    });
 
-map.fitBounds(bounds);
+    var bounds = [[0, 0], [8192, 8192]]; // Image size
 
-// Event-Listener für das Klicken auf die Karte hinzufügen
-map.on('click', function(event) {
-    var latlng = map.mouseEventToLatLng(event.latlng);
-    var marker = L.marker(latlng).addTo(map);
-    marker.bindPopup("Neuer Marker").openPopup();
-});
+    L.imageOverlay('https://www.gtavision.com/images/content/gtav/maps/grand-theft-auto-v_karte-satellit_hq.jpg', bounds).addTo(map);
+
+    map.fitBounds(bounds);
+
+    // Event-Listener für das Klicken auf die Karte hinzufügen
+    map.on('click', function(event) {
+        var latlng = map.mouseEventToLatLng(event.latlng);
+        var marker = L.marker(latlng).addTo(map);
+        marker.bindPopup("Neuer Marker").openPopup();
+    });
+</script>
+
+</body>
+</html>
