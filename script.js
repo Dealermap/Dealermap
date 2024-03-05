9bb787727be4ed632fc24942318432b65626a07c
Dealermap <162360988+Dealermap@users.noreply.github.com>
2024-03-05 19:49:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var map = L.map('map', {
     minZoom: -5
 });
 
-var bounds = [[0, 0], [1536, 2048]]; // Image size
+var bounds = [[0, 0], [8192, 8192]]; // Image size
 
 L.imageOverlay('https://www.gtavision.com/images/content/gtav/maps/grand-theft-auto-v_karte-satellit_hq.jpg', bounds).addTo(map);
 
@@ -11,7 +11,7 @@ map.fitBounds(bounds);
 
 // Add markers for different locations
 var locations = [
-    { name: "Los Santos International Airport", coords: [873, 992] },
+    { name: "Hehler Rockford", coords: [3233, 5292] },
     { name: "Del Perro Beach", coords: [570, 520] },
     // Add more locations as needed
 ];
